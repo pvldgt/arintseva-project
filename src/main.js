9688945dfe14faaaa92dfa93edcafa4317a2462f
Pavel <pvldgt@gmail.com>
2022-12-25 12:17:02 +0300

    deploy test 3

--- src/main.js
+++ src/main.js
@@ -1,1 +1,37 @@
 import './scss/index.scss';
+
+
+// The --active modifier brings out the mobile menu and turns the hamburger lines into a cross
+
+// Create a variable for the hamburger
+const hamburger = document.querySelector(".navbar__hamburger");
+// Create a variable for the menu as a whole 
+const menu = document.querySelector(".navbar__menu");
+
+// add event listener to the hamburger on click
+hamburger.addEventListener("click", mobileMenu);
+
+// function that adds/removes the active modifier class on click
+function mobileMenu() {
+    hamburger.classList.toggle("navbar--active");
+    menu.classList.toggle("navbar--active");
+}
+
+// Now let's close the menu when the user clicks on any of the links in the menu 
+const link = document.querySelectorAll(".navbar__link")
+
+link.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("navbar--active");
+    menu.classList.remove("navbar--active");
+}
+
+
+// The --active-link modifier adds the "hover"-like state to the link from the navbar 
+// when the user scrolls through the corresponding section 
+
+// Default --active-link  on #resume
+// document.querySelector("#resume-link").classList.add("navbar--active-link");
+// Need to read more 
+// https://codepen.io/ifabre/pen/qBZarQ
