b1ef1859639eecd0a34f78917ba1a5c9044e0fdb
Pavel <pvldgt@gmail.com>
2022-12-25 07:27:10 +0300

    deploy test

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    base: "/arintseva-project/",
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                nested: resolve(__dirname, 'en-version.html'),
+            },
+        },
+    },
+})
