27fa04a897908c33c1816ad05b3cb13ae4222b69
Pavel <pvldgt@gmail.com>
2023-01-17 17:17:58 +0300

    Contact form working, added the English page, started working on it

--- mailer-ru.js
+++ mailer-ru.js
@@ -3,36 +3,44 @@
 let btn = document.getElementById('form-button');
 
 // Listen for the submit event 
+// This line prevents the page from reloading and lets the form finish submitting
+
 document.getElementById('contact-form')
   .addEventListener('submit', function (event) {
-    // This line prevents the page from reloading and lets the form finish submitting
     event.preventDefault();
 
+    // Disable the button so that the form isn't submitted multiple times 
+    btn.disabled = true;
+    btn.classList.add("button--disabled")
     // Change the value of the submit button to let the user now that their form is being submitted
     btn.value = 'Отправляется...';
-
     // EmailJS data
-    const serviceID = "service_svs287r";
+    // const serviceID = "service_svs287r";
+    const serviceID = "service_s9wtj9u";
     const templateID = "template_x8hyl2s";
 
     // Send the form 
-    emailjs.sendForm(serviceID, templateID, this, "FhieyttOdI8JteqJU")
+    emailjs.sendForm(serviceID, templateID, this)
       .then(() => {
         // Change the value of the button back to the original and reset the form
         btn.value = "Отправить";
         this.reset();
         // Alert the user that the form has been submitted 
         alert("✅ Ваше сообщение отправлено.");
+        // Enable the button again 
+        btn.disabled = false;
+        btn.classList.remove("button--disabled")
       }, (err) => {
         btn.value = "Отправить";
         this.reset();
         alert(JSON.stringify(err));
+        // Enable the button again 
+        btn.disabled = false;
+        btn.classList.remove("button--disabled")
       });
+
   });
 
-  // Something broke down, try again with CDN in the head tag
-  // test preventing from clicking multiple times
-  // make a separate file for API key and gitignore it 
 
 
 
--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'),
-                nested: resolve(__dirname, 'en-version.html'),
+                nested: resolve(__dirname, 'en.html'),
             },
         },
     },
