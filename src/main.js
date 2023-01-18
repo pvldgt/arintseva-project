4006740bdbc8897fbcde51ae113f55e12a05c093
Pavel <pvldgt@gmail.com>
2023-01-18 06:49:40 +0300

    Remove dev files, clean the comments and code remnants, finish the English page, add padding-bottom for the collapser

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './scss/index.scss';
-// import '../navbar.js';
-// import '../resume-expander.js';
-// import '../mailer-ru.js';
-// import '../mailer-en.js';
+import '../navbar.js';
+import '../resume-expander.js';
+import '../mailer-ru.js';
+import '../mailer-en.js';
 
