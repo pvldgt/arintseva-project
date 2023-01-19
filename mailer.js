fdc463558eaca0c8937b4671605a5c84105d1a4d
Pavel <pvldgt@gmail.com>
2023-01-19 07:04:15 +0300

    Fix the issue where contact form was submitted twice

--- mailer.js
+++ mailer.js
@@ -7,15 +7,17 @@ let btn = document.getElementById('form-button');
 
 document.getElementById('contact-form')
   .addEventListener('submit', function (event) {
+    // This keeps the page from reloading
     event.preventDefault();
+    // This prevents the form from being submitted twice 
+    event.stopImmediatePropagation();
 
     // Disable the button so that the form isn't submitted multiple times 
     btn.disabled = true;
     btn.classList.add("button--disabled")
     // Change the value of the submit button to let the user now that their form is being submitted
-    btn.value = 'Отправляется...';
+    btn.value = btnSending;
     // EmailJS data
-    // const serviceID = "service_svs287r";
     const serviceID = "service_s9wtj9u";
     const templateID = "template_x8hyl2s";
 
@@ -23,15 +25,15 @@ document.getElementById('contact-form')
     emailjs.sendForm(serviceID, templateID, this)
       .then(() => {
         // Change the value of the button back to the original and reset the form
-        btn.value = "Отправить";
+        btn.value = btnSend;
         this.reset();
         // Alert the user that the form has been submitted 
-        alert("✅ Ваше сообщение отправлено.");
+        alert(emailSuccesMessage);
         // Enable the button again 
         btn.disabled = false;
         btn.classList.remove("button--disabled")
       }, (err) => {
-        btn.value = "Отправить";
+        btn.value = btnSend;
         this.reset();
         alert(JSON.stringify(err));
         // Enable the button again 
